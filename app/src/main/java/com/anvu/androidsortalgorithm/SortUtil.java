ead4f16a7570c1ff07f6fece1dd02d67e5797779
anhvule <le.anh.bme@gmail.com>
2017-06-26 18:12:44 +0700

    update bubble sort

--- app/src/main/java/com/anvu/androidsortalgorithm/SortUtil.java
+++ app/src/main/java/com/anvu/androidsortalgorithm/SortUtil.java
@@ -19,17 +19,17 @@ public class SortUtil {
         boolean isSwapped;
 
         for (int i = 0; i < input.length - 1; i++) {
+            isSwapped = false;
             for (int j = 1; j < input.length - 1 - i; j++) {
-                isSwapped = false;
                 if (input[j] < input[j - 1]) {
                     temp = input[j - 1];
-                    input[j - 1] = input[i];
-                    input[i] = temp;
+                    input[j - 1] = input[j];
+                    input[j] = temp;
                     isSwapped = true;
                 }
-                if (!isSwapped) {
-                    return input;
-                }
+            }
+            if (!isSwapped) {
+                return input;
             }
         }
 
